2acd2e4a6791027cdb5471655cc12674436eeb46
Naoki <elmajdoubiyassine5@gmail.com>
2025-11-29 19:50:02 +0100

    add login in the navbar

--- js/auth.js
+++ js/auth.js
@@ -18,3 +18,29 @@ typeButtons.forEach(btn => {
         console.log("Login as:", selectedType);
     });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loggedSection = document.getElementById("logged");
+    const notLoggedSection = document.getElementById("not-logged");
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+        notLoggedSection.style.display = "none";
+        loggedSection.style.display = "flex";
+
+        document.getElementById("navName").textContent = user.name;
+        document.getElementById("navAvatar").src = user.avatar;
+    } else {
+        loggedSection.style.display = "none";
+        notLoggedSection.style.display = "flex";
+    }
+
+    const logoutBtn = document.getElementById("logoutBtn");
+    if (logoutBtn) {
+        logoutBtn.addEventListener("click", () => {
+            localStorage.removeItem("user");
+            location.reload();
+        });
+    }
+});
